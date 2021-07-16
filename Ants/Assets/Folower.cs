b20652e0b76be9bca849469166e62dae371ce8a9
VladimirShat <vovahat00@gmail.com>
2021-07-17 00:22:54 +0500

    Global step by step on plane + sphere

--- Ants/Assets/Folower.cs
+++ Ants/Assets/Folower.cs
@@ -1,7 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 using static Target;
 
 public class Folower : MonoBehaviour
@@ -18,24 +17,20 @@ public class Folower : MonoBehaviour
     RaycastHit rayToTargetHitInfo;
     RaycastHit nextStepHitInfo;
 
-    Vector3 currentPos;
     float distanceToTarget;
 
     // Start is called before the first frame update
     void Start()
     {
-        currentPos = transform.position;
-        nextStep.Rotate(new Vector3(1, 0, 0), nextStepAngle);
     }
 
     // Update is called once per frame
     void Update()
     {
-        currentPos = transform.position;
-        distanceToTarget = Vector3.Distance(currentPos, target.pos);
+        distanceToTarget = Vector3.Distance(transform.position, target.pos);
 
         //next step raycast
-        Ray nextStepRay = new Ray(nextStep.position, nextStep.forward);
+        Ray nextStepRay = new Ray(nextStep.position, -nextStep.up);
         Physics.Raycast(nextStepRay, out nextStepHitInfo, 1, groundMask);
         Debug.DrawRay(nextStepRay.origin, nextStepRay.direction, Color.green);
 
@@ -49,23 +44,23 @@ public class Folower : MonoBehaviour
         if (Physics.Raycast(downRay, out downHitInfo, 1))
         {
             //if folower and target stay on same object
-            if (target.raysDict[Direction.Down].hitInfo.collider.Equals(downHitInfo.collider) && rayToTargetHitInfo.collider.Equals(target.targetCollider))
-            {
-                if (distanceToTarget > 0.1f)
+            //if (target.raysDict[Direction.Down].hitInfo.collider.Equals(downHitInfo.collider) && rayToTargetHitInfo.collider.Equals(target.targetCollider))
+            //{
+                if (distanceToTarget > 0.5f)
                 {
                     transform.position = nextStepHitInfo.point;
                 }
-                transform.position = new Vector3(transform.position.x, transform.position.y + transform.localScale.y / 2, transform.position.z);
-            }
+                transform.position = new Vector3(transform.position.x, downHitInfo.point.y + (transform.localScale.y / 2) + 0.1f, transform.position.z);
+            //}
 
             //rotates perpendicular to the ground
-            transform.rotation = Quaternion.FromToRotation(Vector3.up, downHitInfo.normal);
+            transform.rotation = Quaternion.FromToRotation(transform.up, downHitInfo.normal);
 
-            if(distanceToTarget > 0.1f)
+            if(distanceToTarget > 0.5f)
             {
                 //looks at the target on the x and z axes
                 Vector3 xzTargetPos = target.pos;
-                xzTargetPos.y = currentPos.y;
+                xzTargetPos.y = transform.position.y;
                 transform.LookAt(xzTargetPos);
             }
 
